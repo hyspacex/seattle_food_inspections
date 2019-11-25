3fb454fea5554163be538a8e3dffbee1db6cd007
David <djclancy@uw.edu>
2019-11-25 09:43:48 -0800

    made the cleaning code pep8 compliant

--- code/cleaning.py
+++ code/cleaning.py
@@ -1,85 +1,98 @@
 # -*- coding: utf-8 -*-
 """
-Created on Sun Nov 24 10:29:16 2019
-
-@author: mfardhossein
+cleaning takes the information from the american community survey
+and obtains the information taht is necessary for the project.
 """
 
 
 import pandas as pd
-import matplotlib.pyplot as plt
 
 
 
 
+# Creates a marital status data frame
+DF_MARITAL = pd.read_csv(
+    "../data/raw_data/Marital_Status_ACS_17_5YR_S1201_with_ann.csv")
 
-df_martial=pd.read_csv("C:/Users/mfardhossein/Documents/CS_583/project/Marital_Status_ACS_17_5YR_S1201/ACS_17_5YR_S1201_with_ann.csv")
+print(DF_MARITAL.head())
+#print(DF_MARITAL.columns)
+#print(DF_MARITAL.describe())
+#print(DF_MARITAL.info())
+#print(DF_MARITAL.shape)
 
-print(df_martial.head())
-#print(df_martial.columns)
-#print(df_martial.describe())
-#print(df_martial.info())
-#print(df_martial.shape)
+DF_MARITAL2 = DF_MARITAL[['GEO_id2', 'HC01_EST_VC01', 'HC02_EST_VC01',
+                          'HC03_EST_VC01', 'HC04_EST_VC01', 'HC05_EST_VC01',
+                          'HC06_EST_VC01']]
+DF_MARITAL3 = DF_MARITAL2.iloc[1:]
+DF_MARITAL3.columns = ['zipcode', 'Population', 'No_Married', 'Widowed',
+                       'Divorced', 'Separated', 'Never_Married']
+DF_MARITAL3['No_Married'] = int(DF_MARITAL3['No_Married']) / 100
 
-df_martial2 = df_martial[['GEO_id2', 'HC01_EST_VC01', 'HC02_EST_VC01', 'HC03_EST_VC01', 'HC04_EST_VC01','HC05_EST_VC01','HC06_EST_VC01']]
-df_martial3= df_martial2.iloc[1:]
-df_martial3.columns =['zipcode', 'Population', 'No_Married', 'Widowed','Divorced','Separated','Never_Married']
-df_martial3['No_Married'] = int(df_martial3['No_Married']) /100
-
-df_martial3.loc[df_martial3['No_Married'].str.contains('-'), 'No_Married']=0
-df_martial3 = df_martial3.apply(pd.to_numeric)
+DF_MARITAL3.loc[DF_MARITAL3['No_Married'].str.contains('-'), 'No_Married'] = 0
+DF_MARITAL3 = DF_MARITAL3.apply(pd.to_numeric)
 
 '''
-ss=['No_Married', 'Widowed','Divorced','Separated','Never_Married']
+ss=['No_Married', 'Widowed', 'Divorced', 'Separated', 'Never_Married']
 for j in ss:
-    for i in df_martial3[j]:
-        i=i*df_martial3['Population']        
-    print(df_martial3[j])
+    for i in DF_MARITAL3[j]:
+        i=i*DF_MARITAL3['Population']
+    print(DF_MARITAL3[j])
 '''
-df_martial3['No_Married1']= df_martial3['No_Married'] * df_martial3['Population']
-df_martial3['Widowed1']= df_martial3['Widowed'] * df_martial3['Population']
-df_martial3['Divorced1']= df_martial3['Divorced'] * df_martial3['Population']
-df_martial3['Separated1']= df_martial3['Separated'] * df_martial3['Population']
-df_martial3['Never_Married1']= df_martial3['Never_Married'] * df_martial3['Population']
+DF_MARITAL3['No_Married1'] = (DF_MARITAL3['No_Married']
+                              * DF_MARITAL3['Population'])
+DF_MARITAL3['Widowed1'] = DF_MARITAL3['Widowed'] * DF_MARITAL3['Population']
+DF_MARITAL3['Divorced1'] = DF_MARITAL3['Divorced'] * DF_MARITAL3['Population']
+DF_MARITAL3['Separated1'] = DF_MARITAL3['Separated'] * DF_MARITAL3['Population']
+DF_MARITAL3['Never_Married1'] = (DF_MARITAL3['Never_Married']
+                                 * DF_MARITAL3['Population'])
 
-df_martial3.columns =['zipcode', 'Population', 'No_Married(%)', 'Widowed(%)','Divorced(%)','Separated(%)','Never_Married(%)', 'No_Married', 'Widowed','Divorced','Separated','Never_Married']
+DF_MARITAL3.columns = ['zipcode', 'Population', 'No_Married(%)', 'Widowed(%)',
+                       'Divorced(%)', 'Separated(%)', 'Never_Married(%)',
+                       'No_Married', 'Widowed', 'Divorced', 'Separated',
+                       'Never_Married']
 
-df_income=pd.read_csv("C:/Users/mfardhossein/Documents/CS_583/project/Median_Household_Income_ACS_17_5YR_S1903/ACS_17_5YR_S1903_with_ann.csv")
+DF_INCOME = pd.read_csv(
+    "../data/raw_data/Median_Household_Income_ACS_17_5YR_S1903_with_ann.csv")
 
-print(df_income.head())
-#print(df_income.columns)
-#print(df_income.describe())
-#print(df_income.info())
-#print(df_income.shape)
+print(DF_INCOME.head())
+#print(DF_INCOME.columns)
+#print(DF_INCOME.describe())
+#print(DF_INCOME.info())
+#print(DF_INCOME.shape)
 
-df_income2 = df_income[['GEO.id2', 'HC01_EST_VC02', 'HC02_EST_VC02', 'HC03_EST_VC02']]
-df_income3= df_income2.iloc[1:]
-df_income3.columns =['zipcode', 'Number_Households', 'Precent_Distribution_Housesholds', 'Median_Income_Households']
+DF_INCOME2 = DF_INCOME[['GEO.id2', 'HC01_EST_VC02', 'HC02_EST_VC02',
+                        'HC03_EST_VC02']]
+DF_INCOME3 = DF_INCOME2.iloc[1:]
+DF_INCOME3.columns = ['zipcode', 'Number_Households',
+                      'Precent_Distribution_Housesholds',
+                      'Median_Income_Households']
 
-df_income3.loc[df_income3['No_Married'].str.contains('-'), 'No_Married']=df_income3['Number_Households'].mean()
-df_income3 = df_income3.apply(pd.to_numeric)
+DF_INCOME3.loc[DF_INCOME3['No_Married'].str.contains('-'),
+               'No_Married'] = DF_INCOME3['Number_Households'].mean()
+DF_INCOME3 = DF_INCOME3.apply(pd.to_numeric)
 
 
 ###Joining
-df = pd.merge(left=df_martial3, right=df_income3, left_on='zipcode', right_on='zipcode')
+DF = pd.merge(left=DF_MARITAL3, right=DF_INCOME3, left_on='zipcode',
+              right_on='zipcode')
 
 
-print(df_martial.isnull().sum())
-#print(df_martial.isnull().values.any())
+print(DF_MARITAL.isnull().sum())
+#print(DF_MARITAL.isnull().values.any())
 
-df.to_csv('C:/Users/mfardhossein/Documents/CS_583/project/Marital_Status_ACS_17_5YR_S1201/trial.csv')
+DF.to_csv('"../data/raw_data/trial.csv')
 
-df_food=pd.read_csv("C:/Users/mfardhossein/Documents/CS_583/project/Food_Establishment_Inspection_Data (3).csv")
+DF_FOOD = pd.read_csv("../data/raw_data/Food_Establishment_Inspection_Data.csv")
 
-df_food['Zip Code']= df_food['Zip Code'].astype(str).str.zfill(5)
+DF_FOOD['Zip Code'] = DF_FOOD['Zip Code'].astype(str).str.zfill(5)
 
-zip_food = list(df_food['Zip Code'].values())
+ZIP_FOOD = list(DF_FOOD['Zip Code'].values())
 
 '''
-for i in zip_food:
-    if i in df.zipcode:
+for i in ZIP_FOOD:
+    if i in DF.zipcode:
         print(i)
 '''
 
 
-df['zipcode'].isin(df_food['Zip Code']).value_counts()
+DF['zipcode'].isin(DF_FOOD['Zip Code']).value_counts()
