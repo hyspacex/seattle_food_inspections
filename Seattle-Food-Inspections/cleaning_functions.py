b7c6b9e4704e5a639d339e96da1a8a1ea55c6256
David <djclancy@uw.edu>
2019-12-09 18:22:47 -0800

    rename main folder

---
+++ Seattle-Food-Inspections/cleaning_functions.py
@@ -0,0 +1,107 @@
+'''
+Python code to clean the census and food inspection data.
+'''
+
+def dataframe_with_columns(data_frame, columns_list, new_names):
+    '''
+    Extracts certain columns from the dataframe and changes the names of
+    the columns
+
+    Args:
+        data_frame (dataframe): dataframe to be cleaned
+        columns_list (list of str): list of columns to keep in output
+        new_names (list of str): list of desired column names for output
+
+    Returns:
+        data_frame_subset: cleaned dataframe
+    '''
+    data_frame_subset = data_frame[columns_list] #extracts columns
+    data_frame_subset.columns = new_names #renames columns
+    return data_frame_subset
+
+
+def remove_row_from_data_frame(data_frame):
+    '''
+    Removes the top row from a dataframe.
+
+    Args:
+        data_frame (dataframe): dataframe to be cleaned
+
+    Returns:
+        data_frame: cleaned dataframe
+    '''
+    return data_frame.iloc[1:]
+
+def remove_dashes_from_data(data_frame, col):
+    '''
+    Remove the dashes from the dataset, but only
+    looks at the columns in "cols"
+
+    Args:
+        data_frame (dataframe): dataframe to be cleaned
+        col (list of str): list of columns to be cleaned
+
+    Returns:
+        data_frame: cleaned dataframe
+    '''
+    for col_name in col:
+        vec = list(data_frame[col_name])
+        for vec_local, vec_elem in enumerate(vec):
+            if vec_elem == "-":
+                vec[vec_local] = 0
+        data_frame[col_name] = vec
+    return data_frame
+
+def columns_to_float(data_frame, col):
+    '''
+    Convertes a collection of columns in a dataframe to a
+    float type.
+
+    Args:
+        data_frame (dataframe): dataframe to be cleaned
+        col (list of str): list of columns to be cleaned
+
+    Returns:
+        data_frame: cleaned dataframe
+    '''
+    for col_name in col:
+        data_frame[col_name] = [float(f) for f in data_frame[col_name]]
+    return data_frame
+
+def columns_to_int(data_frame, col):
+    '''
+    Convertes a collection of columns in a dataframe to a
+    int type.
+
+    Args:
+        data_frame (dataframe): dataframe to be cleaned
+        col (list of str): list of columns to be cleaned
+
+    Returns:
+        data_frame: cleaned dataframe
+    '''
+    for col_name in col:
+        data_frame[col_name] = [int(f) for f in data_frame[col_name]]
+    return data_frame
+
+def data_from_percents_and_raw_totals(data_frame, column_list,
+                                      tot_row, new_list):
+    '''
+    The dataframe contains information of percents (in the column_list) and
+    a total variable (in tot_row). This creates new columns (labeled by
+    new_list) which is the row-wise product of percent and total variable.
+
+    Args:
+        data_frame (dataframe): dataframe to be cleaned
+        column_list (list of str): list of columns to be cleaned
+        tot_row (str): column that contains total values
+        new_list (list of str): list of column names
+
+    Returns:
+        data_frame: cleaned dataframe
+    '''
+    col_length = len(column_list)
+    for j in range(0, col_length):
+        data_frame[new_list[j]] = (data_frame[column_list[j]]
+                                   * data_frame[tot_row]) / 100
+    return data_frame
---
+++ Seattle-Food-Inspections/create_clean_data.py
@@ -0,0 +1,126 @@
+'''
+Cleans and combines the census and INSPECTION data.
+'''
+#Import statements
+import pandas as pd
+from . import cleaning_functions as cf
+from . import merging_functions as mf
+
+#Constants
+NAME = 'Name'
+CITY = 'City'
+VIOLATIONTYPE = 'Violation Type'
+ZIPCODE = 'zipcode'
+LONGITITUDE = 'Longitude'
+INSPECTDATE = 'Inspection Date'
+RESTAURANTNAME = 'Program Identifier'
+ADDRESS = 'Address'
+GRADE = 'Grade'
+SEATTLEZIPS = [98101, 98102, 98103, 98104, 98105, 98106, 98107, 98108, 98109,
+               98112, 98115, 98116, 98117, 98118, 98119, 98121, 98122, 98125,
+               98126, 98133, 98134, 98136, 98144, 98146, 98154, 98155, 98164,
+               98168, 98174, 98177, 98178, 98195, 98199]
+
+### Marital dataframe creation
+DF_MARITAL = pd.read_csv(
+    './data/raw_data/Marital_ACS_17_5YR_S1201_with_ann.csv')
+# Removes the top row, which is useless
+DF_MARITAL = cf.remove_row_from_data_frame(DF_MARITAL)
+# Extracts and rename the proper columns
+DF_MARITAL = cf.dataframe_with_columns(DF_MARITAL,
+                                       ['GEO.id2', 'HC01_EST_VC01',
+                                        'HC02_EST_VC01', 'HC03_EST_VC01',
+                                        'HC04_EST_VC01', 'HC05_EST_VC01',
+                                        'HC06_EST_VC01'],
+                                       ['zipcode', 'Population',
+                                        'No_Married(%)', 'Widowed(%)',
+                                        'Divorced(%)', 'Separated(%)',
+                                        'Never_Married(%)'])
+# Defining an oft used list of column names.
+PERCENT_COLUMNS = ['No_Married(%)', 'Widowed(%)', 'Divorced(%)', 'Separated(%)',
+                   'Never_Married(%)']
+# Remove dashes from the data
+DF_MARITAL = cf.remove_dashes_from_data(DF_MARITAL, PERCENT_COLUMNS)
+# Turns the percents to floats and the population number to an int
+DF_MARITAL = cf.columns_to_float(DF_MARITAL, PERCENT_COLUMNS)
+DF_MARITAL = cf.columns_to_int(DF_MARITAL, ['Population', 'zipcode'])
+# Defining another colection of column names.
+RAW_TOT_COLUMNS = ['No_Married', 'Widowed', 'Divorced', 'Separated',
+                   'Never_Married']
+# Create the new columns which contain the raw totals for all the
+# statistics in the marriage center.
+DF_MARITAL = cf.data_from_percents_and_raw_totals(DF_MARITAL, PERCENT_COLUMNS,
+                                                  "Population",
+                                                  RAW_TOT_COLUMNS)
+DF_MARITAL = cf.columns_to_int(DF_MARITAL, RAW_TOT_COLUMNS)
+
+
+
+### INCOME INFORMATION
+DF_INCOME = pd.read_csv("./data/raw_data/Income_ACS_17_5YR_S1903_with_ann.csv")
+# Remove an extra row
+DF_INCOME = cf.remove_row_from_data_frame(DF_INCOME)
+# Extract the useful rows
+DF_INCOME = cf.dataframe_with_columns(DF_INCOME,
+                                      ['GEO.id2', 'HC01_EST_VC02',
+                                       'HC02_EST_VC02', 'HC03_EST_VC02'],
+                                      ['zipcode', 'Number_Households',
+                                       'Percent_Distribution_Housesholds',
+                                       'Median_Income_Households'])
+# Remove dashes from the data, sets them as zero.
+DF_INCOME = cf.remove_dashes_from_data(DF_INCOME,
+                                       ['Number_Households',
+                                        'Percent_Distribution_Housesholds',
+                                        'Median_Income_Households'])
+# zipcode to integer for merging purposes
+DF_INCOME = cf.columns_to_int(DF_INCOME, ['zipcode'])
+
+### Food INSPECTION data frame creation
+INSPECTION = pd.read_csv('./data/raw_data/Food_Establishment_INSPECTION.csv',
+                         low_memory=False)
+#rename columns for consistency
+INSPECTION.rename(columns={'Zip Code': ZIPCODE}, inplace=True)
+# convert dates in 'INSPECTION Date' to date time
+INSPECTION[INSPECTDATE] = pd.to_datetime(INSPECTION[INSPECTDATE])
+# sort by INSPECTION date then remove duplicates
+INSPECTION = INSPECTION.sort_values(
+    by=[INSPECTDATE]
+).drop_duplicates(subset=[RESTAURANTNAME, ADDRESS], keep='last')
+# dropping unnecessary columns
+INSPECTION = INSPECTION.drop(
+    columns=[
+        "Inspection_Serial_Num",
+        "Violation_Record_ID",
+        "Phone",
+        "Program Identifier"
+    ]
+)
+# drop na/nan values in appropriate columns
+INSPECTION_CLEANED = INSPECTION.dropna(subset=[LONGITITUDE])
+INSPECTION_CLEANED = INSPECTION_CLEANED.dropna(subset=[GRADE])
+INSPECTION_CLEANED = INSPECTION_CLEANED.dropna(subset=[ZIPCODE])
+INSPECTION_CLEANED = INSPECTION_CLEANED.dropna(subset=[NAME])
+INSPECTION_CLEANED = INSPECTION_CLEANED.dropna(subset=[CITY])
+INSPECTION_CLEANED = INSPECTION_CLEANED.dropna(subset=[VIOLATIONTYPE])
+INSPECTION_CLEANED = INSPECTION_CLEANED.sort_values(by=[INSPECTDATE])
+# Capitalizing the columns to prevent duplication in enumerating
+INSPECTION_CLEANED[CITY] = INSPECTION_CLEANED[CITY].str.upper()
+INSPECTION_CLEANED[NAME] = INSPECTION_CLEANED[NAME].str.upper()
+INSPECTION_CLEANED[VIOLATIONTYPE] = INSPECTION_CLEANED[VIOLATIONTYPE].str.upper()
+# convert the data type of Zip Code to integer
+INSPECTION_CLEANED[ZIPCODE] = INSPECTION_CLEANED[ZIPCODE].astype(int)
+INSPECTION_ZIPS = INSPECTION_CLEANED[
+    INSPECTION_CLEANED[ZIPCODE].isin(SEATTLEZIPS)]
+INSPECTION_ZIPS.to_csv('./data/clean_data/clean_inspection.csv', index=False)
+
+
+
+##COMBINE DATAFRAMES
+# first, combine census data
+DF_TOTAL = mf.merge_dataframes(DF_MARITAL, DF_INCOME, 'zipcode')
+#output combined census data
+DF_TOTAL.to_csv('./data/clean_data/clean_census.csv', index=False)
+
+# next, combine census data with food inspection dataset
+COMBINED = mf.merge_dataframes(INSPECTION_ZIPS, DF_TOTAL, ZIPCODE)
+COMBINED.to_csv('./data/clean_data/combined.csv', index=False)
