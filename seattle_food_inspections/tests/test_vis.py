01587281004047d47f7aafce52021ee9104ee563
David <djclancy@uw.edu>
2019-12-09 19:43:53 -0800

    test_vis added

---
+++ seattle_food_inspections/tests/test_vis.py
@@ -0,0 +1,15 @@
+'''
+Tests for the visualization.py file
+'''
+# Now you can import your module
+
+# Import the cleaning_functions
+from .. import visualization as vz
+
+def test_folium():
+    test_map = vz.make_folium_map(True)
+    assert str(type(map)) == "<class 'folium.folium.Map'>"
+
+def test_altair():
+    t_info = vz.make_altair_map('income')
+    assert str(type(t_info)) == "<class 'altair.vegalite.v3.api.HConcatChart'>"
